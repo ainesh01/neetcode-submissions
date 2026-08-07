e69a13f1aa5135e92526ffe0e476d459469587ad
Ainesh Arumugam <ainesh01@gmail.com>
2026-08-06 23:22:12 -0700

    Add: climbing-stairs - submission-0

---
+++ Data Structures & Algorithms/climbing-stairs/submission-0.py
@@ -0,0 +1,23 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        memo = [0]*(n+1)
+        memo[0] = 0
+        memo[1] = 1
+        memo[2] = 2
+        def recurse(n: int) -> int:
+            if memo[n]!=0:
+                return memo[n]
+            res = recurse(n-1)+recurse(n-2)
+            memo[n] = res
+            return res
+        
+
+        return recurse(n)
+
+
+
+        
+
+
+
+        
